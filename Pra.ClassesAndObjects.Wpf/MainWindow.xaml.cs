afb979c40bf28a082116beeb9d24a517a1757dc6
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-05-02 14:07:09 +0200

    Rename project and namespace

--- Pra.ClassesAndObjects.Wpf/MainWindow.xaml.cs
+++ Pra.ClassesAndObjects.Wpf/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Prb.ClassesAndObjects.Wpf
+namespace Pra.ClassesAndObjects.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
